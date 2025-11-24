707a91523a3f62b230a3d5fe2399852c97ddba91
unknown <anajmp11@gmail.com>
2025-11-24 12:23:14 -0300

    primeiro commit

--- src/models/pedidosModel.js
+++ src/models/pedidosModel.js
@@ -10,10 +10,7 @@ const pedidosModel = {
         valor_base_km,
         valor_base_kg
     ) => {
-        const sql = `
-      INSERT INTO pedidos
-      (id_cliente_fk, data_pedido, tipo_entrega, distancia_km, peso_kg, valor_base_km, valor_base_kg)
-      VALUES (?, ?, ?, ?, ?, ?, ?)
+        const sql = `INSERT INTO pedidos (id_cliente_fk, data_pedido, tipo_entrega, distancia_km, peso_kg, valor_base_km, valor_base_kg) VALUES (?, ?, ?, ?, ?, ?, ?)
     `;
         const values = [
             id_cliente_fk,
